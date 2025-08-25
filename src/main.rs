9d9c551a1e64ea3cf2f25f4905814482f1e19334
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-08-25 18:00:39 +0200

    chore: Log args received

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    for arg in args.iter() {
+      println!("Received arg: {}", arg);
+    }
 }
